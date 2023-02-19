77713a73dfb8fcd097902110b1e00f1e23b620fd
Muhammad Sajidul islam <sajid285.me@gmail.com>
2023-02-19 22:23:10 +0600

    first commit

---
+++ resources/js/vue/backend/views/product/Create.vue
@@ -0,0 +1,151 @@
+<template>
+    <div class="container">
+        <div class="card list_card">
+            <div class="card-header">
+                <h4>Create</h4>
+                <div class="btns">
+                    <router-link :to="{ name: `All${route_prefix}` }" class="btn rounded-pill btn-outline-warning" >
+                        <i class="fa fa-arrow-left me-5px"></i>
+                        Back
+                    </router-link>
+                </div>
+            </div>
+            <form @submit.prevent="call_store(`store_${store_prefix}`,$event.target)" autocomplete="false">
+                <div class="card-body">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-10">
+                            <div class="admin_form form_1">
+                                <div class=" form-group d-grid align-content-start gap-1 mb-2 " >
+                                    <input-field
+                                        :label="`Name`"
+                                        :name="`full_name`"
+                                    />
+                                </div>
+                                <div class="form-group d-grid align-content-start gap-1 mb-2 " >
+                                    <input-field
+                                        :label="`Default price`"
+                                        :name="`default_price`"
+                                    />
+                                </div>
+                                <div class=" form-group d-grid align-content-start gap-1 mb-2 " >
+                                    <label for="category_id">Category</label>
+                                    <select name="category_id" id="category_id" class="form-control">
+                                        <option value="">Computer</option>
+                                        <option value="">Laptop</option>
+                                        <option value="">Monitor</option>
+                                        <option value="">Desktop component</option>
+                                    </select>
+                                    <!-- <textarea class="form-control" id="message" name="message"></textarea> -->
+                                </div>
+
+                                <div class="form-group d-grid align-content-start gap-1 mb-2 " >
+                                    <input-field
+                                        :label="`Image`"
+                                        :name="`image`"
+                                        :type="`file`"
+                                        :preview="true"
+                                    />
+                                </div>
+
+                                <div class="form-group d-grid align-content-start gap-1 mb-2 " >
+                                    <input-field
+                                        :label="`stock`"
+                                        :name="`stock`"
+                                        :type="`number`"
+                                    />
+                                </div>
+
+                                <div class="form-group d-grid align-content-start gap-1 mb-2 " >
+                                    <input-field
+                                        :label="`low stock`"
+                                        :name="`low_stock`"
+                                        :type="`number`"
+                                    />
+                                </div>
+
+                                <div class="form-group d-grid align-content-start gap-1 mb-2 " >
+                                    <input-field
+                                        :label="`search_keyword`"
+                                        :name="`search_keyword`"
+                                    />
+                                </div>
+
+                                <div class="form-group d-grid align-content-start gap-1 mb-2 " >
+                                    <label for="description">Description</label>
+                                    <textarea class="form-control" id="description" name="description"></textarea>
+                                </div>
+                                <div class="seo_section full_width mt-5">
+                                    
+                                    <div class="heading mb-4">
+                                        <h4 class="d-flex justify-content-center">Seo section</h4>
+                                        <h6 class="d-flex justify-content-center">Boost traffic to your online business.</h6>
+                                    </div>
+                                    <hr>
+                                    <div class="admin_form form_1 col_2 mt-3">
+                                        <div class="form-group d-grid align-content-start gap-1 mb-2 ">
+                                            <input-field
+                                                :label="`Page Title`"
+                                                :name="`page_title`"
+                                            />
+                                        </div>
+
+                                        <div class="form-group d-grid align-content-start gap-1 mb-2 " >
+                                            <input-field
+                                                :label="`product url`"
+                                                :name="`product_url`"
+                                            />
+                                        </div>
+
+                                        <div class="form-group full_width d-grid align-content-start gap-1 mb-2 " >
+                                            <label for="meta_description">Meta Description</label>
+                                            <textarea class="form-control" id="meta_description" name="meta_description"></textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer text-center">
+                    <button type="submit" class="btn btn-outline-info" >
+                        <i class="fa fa-upload"></i>
+                        Submit
+                    </button>
+                </div>
+            </form>
+        </div>
+        <CategoryManagementModal/>
+    </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+import InputField from '../components/InputField.vue'
+/** store and route prefix for export object use */
+import PageSetup from './PageSetup';
+import CategoryManagementModal from "../category/components/ManagementModal.vue"
+const {route_prefix, store_prefix} = PageSetup;
+
+export default {
+    components: { InputField, CategoryManagementModal },
+    data: function(){
+        return {
+            /** store prefix for JSX */
+            store_prefix,
+            route_prefix
+        }
+    },
+    created: function () {},
+    methods: {
+        ...mapActions([`store_${store_prefix}`]),
+        call_store: function(name, params=null){
+            this[name](params)
+        },
+    },
+};
+</script>
+
+<style>
+</style>
---
+++ routes/api.php
@@ -0,0 +1,111 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::group(
+    ['prefix' => 'v1', 'namespace' => 'Controllers'],
+    function () {
+        Route::group(['prefix' => '/user', 'middleware' => ['guest:api']], function () {
+            Route::post('/get-token', 'Auth\ApiLoginController@get_token');
+            Route::post('/api-login', 'Auth\ApiLoginController@login');
+            Route::post('/api-register', 'Auth\ApiLoginController@register');
+            Route::get('/auth-check', 'Auth\ApiLoginController@auth_check');
+            Route::post('/forget-mail', 'Auth\ApiLoginController@forget_mail');
+            Route::post('/check-code', 'Auth\ApiLoginController@check_code');
+            Route::post('/logout-from-all-devices', 'Auth\ApiLoginController@logout_from_all_devices');
+        });
+
+        Route::group(['middleware' => ['auth:api']], function () {
+            Route::group(['prefix' => 'user'], function () {
+                Route::post('/api-logout', 'Auth\ApiLoginController@logout');
+                Route::post('/user_info', 'Auth\ApiLoginController@user_info');
+                Route::post('/check-auth', 'Auth\ApiLoginController@check_auth');
+                Route::post('/user_update', 'Auth\ApiLoginController@user_update');
+                Route::post('/update_password', 'Auth\ApiLoginController@update_password');
+                Route::post('/find-user-info', 'Auth\ApiLoginController@find_user_info');
+            });
+
+            Route::group(['prefix' => 'user'], function () {
+                Route::post('/update-profile', 'Auth\ProfileController@update_profile');
+                Route::get('/all','Auth\UserController@all');
+                Route::post('/store','Auth\UserController@store');
+                Route::post('/canvas-store','Auth\UserController@canvas_store');
+                Route::post('/update','Auth\UserController@update');
+                Route::post('/soft-delete','Auth\UserController@soft_delete');
+                Route::post('/destroy','Auth\UserController@destroy');
+                Route::post('/restore','Auth\UserController@restore');
+                Route::post('/bulk-import','Auth\UserController@bulk_import');
+                Route::get('/{id}','Auth\UserController@show');
+            });
+
+            Route::group(['prefix' => 'user-role'], function () {
+                Route::get('/all','Auth\UserRoleController@all');
+                Route::post('/store','Auth\UserRoleController@store');
+                Route::post('/canvas-store','Auth\UserRoleController@canvas_store');
+                Route::post('/update','Auth\UserRoleController@update');
+                Route::post('/canvas-update','Auth\UserRoleController@canvas_update');
+                Route::post('/soft-delete','Auth\UserRoleController@soft_delete');
+                Route::post('/destroy','Auth\UserRoleController@destroy');
+                Route::post('/restore','Auth\UserRoleController@restore');
+                Route::post('/bulk-import','Auth\UserRoleController@bulk_import');
+                Route::get('/{id}','Auth\UserRoleController@show');
+            });
+
+            Route::group(['prefix' => 'contact-message'], function () {
+                Route::get('/all','Admin\ContactMessageController@all');
+                Route::post('/store','Admin\ContactMessageController@store');
+                Route::post('/canvas-store','Admin\ContactMessageController@canvas_store');
+                Route::post('/update','Admin\ContactMessageController@update');
+                Route::post('/canvas-update','Admin\ContactMessageController@canvas_update');
+                Route::post('/soft-delete','Admin\ContactMessageController@soft_delete');
+                Route::post('/destroy','Admin\ContactMessageController@destroy');
+                Route::post('/restore','Admin\ContactMessageController@restore');
+                Route::post('/bulk-import','Admin\ContactMessageController@bulk_import');
+                Route::get('/{id}','Admin\ContactMessageController@show');
+            });
+           
+            Route::group(['prefix' => 'product'], function () {
+                Route::get('/all','Admin\Product\ProductController@all');
+                Route::post('/store','Admin\Product\ProductController@store');
+                Route::post('/canvas-store','Admin\Product\ProductController@canvas_store');
+                Route::post('/update','Admin\Product\ProductController@update');
+                Route::post('/canvas-update','Admin\Product\ProductController@canvas_update');
+                Route::post('/soft-delete','Admin\Product\ProductController@soft_delete');
+                Route::post('/destroy','Admin\Product\ProductController@destroy');
+                Route::post('/restore','Admin\Product\ProductController@restore');
+                Route::post('/bulk-import','Admin\Product\ProductController@bulk_import');
+                Route::get('/{id}','Admin\Product\ProductController@show');
+            });
+
+            Route::group(['prefix' => 'category'], function () {
+                Route::get('/all','Admin\Product\CategoryController@all');
+                Route::post('/store','Admin\Product\CategoryController@store');
+                Route::post('/canvas-store','Admin\Product\CategoryController@canvas_store');
+                Route::post('/update','Admin\Product\CategoryController@update');
+                Route::post('/canvas-update','Admin\Product\CategoryController@canvas_update');
+                Route::post('/soft-delete','Admin\Product\CategoryController@soft_delete');
+                Route::post('/destroy','Admin\Product\CategoryController@destroy');
+                Route::post('/restore','Admin\Product\CategoryController@restore');
+                Route::post('/bulk-import','Admin\Product\CategoryController@bulk_import');
+                Route::get('/{id}','Admin\Product\CategoryController@show');
+            });
+
+        });
+    }
+);
